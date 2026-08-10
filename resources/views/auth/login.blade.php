a953e27fb56a894ecd615889db9b4c849c2ba80c
Shakil Mahmud <shakil.regnum@gmail.com>
2026-08-10 13:12:27 +0600

    Fix duplicate Sign in text on login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
         <div class="card overflow-hidden account-card mx-3">
             <div class="bg-primary p-4 text-white text-center position-relative">
                 <h4 class="font-20 m-b-5">Welcome Back !</h4>
-                <p class="text-white-50 mb-4">Sign in Sign in as Admin to AMS</p>
+                <p class="text-white-50 mb-4">Sign in as Admin to AMS</p>
                 <a href="{{ route('welcome') }}" class="logo logo-admin">
                     <h1>A</h1>
                 </a>
