caee6fc85605949ca87d57f916e302fd76268547
Shakil Mahmud <shakil.regnum@gmail.com>
2026-08-10 14:53:50 +0600

    Protect FingerHelper against missing sockets extension and constructor exceptions

--- app/Helpers/FingerHelper.php
+++ app/Helpers/FingerHelper.php
@@ -6,13 +6,25 @@ use Rats\Zkteco\Lib\ZKTeco;
 
 class FingerHelper
 {
-    public function init($ip, $port = 4370): ZKTeco
+    public function init($ip, $port = 4370): ?ZKTeco
     {
-        return new ZKTeco($ip, $port);
+        if (!function_exists('socket_create')) {
+            return null;
+        }
+
+        try {
+            return new ZKTeco($ip, $port);
+        } catch (\Throwable $e) {
+            return null;
+        }
     }
 
-    public function getStatus(ZKTeco $zk): bool
+    public function getStatus(?ZKTeco $zk): bool
     {
+        if (!$zk) {
+            return false;
+        }
+
         try {
             return @$zk->connect();
         } catch (\Throwable $e) {
@@ -20,13 +32,17 @@ class FingerHelper
         }
     }
 
-    public function getStatusFormatted(ZKTeco $zk): string
+    public function getStatusFormatted(?ZKTeco $zk): string
     {
         return $this->getStatus($zk) ? "Active" : "Deactivated";
     }
 
-    public function getSerial(ZKTeco $zk)
+    public function getSerial(?ZKTeco $zk)
     {
+        if (!$zk) {
+            return false;
+        }
+
         try {
             if ($this->getStatus($zk)) {
                 $serial = $zk->serialNumber();
